4729ed2a29750525c01c54fb17668394e01b0960
Cala Torra <000404667@bga.upb>
2019-03-29 12:48:24 -0500

    primer

---
+++ mainPotencia.py
@@ -0,0 +1,22 @@
+#definir funcion
+def a_power_b (a,b):
+    prod=1
+    for x in range (0,b):
+        prod=prod*a
+        return prod
+
+#codigo
+
+base=int(input("ingrese la base"))
+exponente=int(input("ingrese el exponente"))
+
+result=a_power_b(base,exponente)
+
+print("el resultado de su potencia es: ",result)
+
+
+
+
+
+
+    
