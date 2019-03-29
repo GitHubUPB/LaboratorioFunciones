d7a292fddfac024b64658ac100961bfdbd41cc02
Cala Torra <000404667@bga.upb>
2019-03-29 13:41:58 -0500

    tercero

--- mainPotencia.py
+++ mainPotencia.py
@@ -1,8 +1,11 @@
 #definir funcion
 def a_power_b (a,b):
     prod=1
-    for x in range (1,b):
-        prod=prod*b
+    for x in range (1,b+1):
+        if x>=1000:
+            raise StopIteration("Pailas")
+        
+        prod=prod*a
     return prod
 
 
@@ -10,16 +13,23 @@ def a_power_b (a,b):
 
 while True:
     
-    base=int(input("ingrese la base"))
-    if base ==0:
-        print("numero es cero ####")
-        break
-    exponente=int(input("ingrese el exponente"))
-    result=a_power_b(base,exponente)
-    print("el resultado de su potencia es: ",result)
-    if base ==0:
-        break
+    try:
+        base=int(input("ingrese la base"))
+        if base ==0:
+            print("numero es cero ####")
+            break
+        exponente=int(input("ingrese el exponente"))
+    except ValueError:
+        print("ingreso una letra")
+    except StopIteration:
+        print("sobrepaso el limite")
 
+    
+    
+
+    
+   
+ 
 
 
 
