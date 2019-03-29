9e4c3a97546f224b052f07f69caf99d20bb8bfd4
Cala Torra <000404667@bga.upb>
2019-03-29 14:11:26 -0500

    ejercicios uno por uno

---
+++ mainPotenciaDos.py
@@ -0,0 +1,20 @@
+def a_power_b (a,b):
+    prod=1
+    for x in range (0,b):
+        prod=prod*a
+    return prod
+
+
+#codigo
+
+while True:
+    
+    base=int(input("ingrese la base"))
+    if base ==0:
+        print("numero es cero ####")
+        break
+    exponente=int(input("ingrese el exponente"))
+    result=a_power_b(base,exponente)
+    print("el resultado de su potencia es: ",result)
+    if base ==0:
+        break
---
+++ mainPotenciaTres.py
@@ -0,0 +1,27 @@
+#definir funcion
+def a_power_b (a,b):
+    prod=1
+    for x in range (1,b+1):
+        if x>=1000:
+            raise StopIteration("Pailas")
+        
+        prod=prod*a
+    return prod
+
+
+#codigo
+
+while True:
+    
+    try:
+        base=int(input("ingrese la base"))
+        if base ==0:
+            print("numero es cero ####")
+            break
+        exponente=int(input("ingrese el exponente"))
+        result=a_power_b(base,exponente)
+        print("el resultado de su potencia es: ",result)
+    except ValueError:
+        print("ingreso una letra")
+    except StopIteration:
+        print("sobrepaso el limite")
